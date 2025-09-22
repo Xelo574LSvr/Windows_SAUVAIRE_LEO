6ba6c559270932e57d1e64589128cbd905ec7e78
Xelo574LSvr <leo.sauvaire@iut-rodez.fr>
2025-09-22 14:20:39 +0000

    Correction classe image et gradient

--- Windows_SAUVAIRE_LEO/LectureEcritureJava/Gradient.java
+++ Windows_SAUVAIRE_LEO/LectureEcritureJava/Gradient.java
@@ -1,20 +1,23 @@
+// Programme principal : crée un dégradé
 public class Gradient {
-    public static void main(String[] args) {
-        Image img = new Image(200, 100);
+  public static void main(String[] args) {
+    int largeur = 200;
+    int hauteur = 100;
+    Image img = new Image(largeur, hauteur);
 
-        // Génération du dégradé de bleu
-        for (int y = 0; y < hauteur; y++) {
-            for (int x = 0; x < largeur; x++) {
-                int bleu = 0; // quel calcule ?
-                img.setPixel(x, y, 0, 0, bleu);
-            }
-        }
-
-        try {
-            img.save("gradient.ppm");
-            System.out.println("Dégradé créé avec succès !");
-        } catch (Exception e) {
-            System.err.println("Erreur lors de la création du dégradé : " + e.getMessage());
-        }
+    // Génération du dégradé de bleu (de 0 à 255)
+    for (int y = 0; y < hauteur; y++) {
+      for (int x = 0; x < largeur; x++) {
+        int bleu = (int) (255.0 * x / (largeur - 1)); 
+        img.setPixel(x, y, 0, 0, bleu);
+      }
     }
+
+    try {
+      img.save_txt("gradient.ppm");
+      System.out.println("Dégradé créé avec succès !");
+    } catch (Exception e) {
+      System.err.println("Erreur lors de la création du dégradé : " + e.getMessage());
+    }
+  }
 }
--- Windows_SAUVAIRE_LEO/LectureEcritureJava/Image.java
+++ Windows_SAUVAIRE_LEO/LectureEcritureJava/Image.java
@@ -1,3 +1,4 @@
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
@@ -35,8 +36,23 @@ public class Image {
      * Sauvegarde l'image au format texte PPM (P3)
      */
     public void save_txt(String filename) throws IOException {
-        // TODO : écrire le fichier PPM avec FileWriter
+        try (FileWriter writer = new FileWriter(filename)) {
+            writer.write("P3\n");
+            writer.write(width + " " + height + "\n");
+            writer.write("255\n");
+
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                int r = pixels[y][x][0];
+                int g = pixels[y][x][1];
+                int b = pixels[y][x][2];
+                writer.write(r + " " + g + " " + b + " ");
+                }
+                writer.write("\n");
+            }
+        }
     }
+
 
 
     static public read_txt(String filename) throws IOException { 
@@ -62,6 +78,92 @@ public class Image {
                 img.setPixel(x, y, r, g, b);
             }
         }
+    }
 
+    
+
+    
+
+    public static Image read_txt_token(String filename) throws IOException {
+        // Lire tout le fichier
+        FileInputStream fis = new FileInputStream(filename);
+        byte[] data = fis.readAllBytes();
+        fis.close();
+
+        // Question : faut-il gérer l'encodage ici ?
+        String content = new String(data);
+
+        // --- Première passe : compter le nombre de tokens ---
+        
+        int tokenCount = 0;
+        int lastPos = 0;
+        
+        for (int i = 0; i < content.length(); i++) {
+            char c = content.charAt(i);
+            if (Character.isWhitespace(c)) {
+            tokenCount++;
+            lastPos = i + 1;
+            }
+        }
+        
+        if (lastPos < content.length()) { // dernier token
+            tokenCount++;
+        }
+
+        // --- Deuxième passe : extraire les tokens ---
+        
+        String[] tokens = new String[tokenCount];
+        int index = 0;
+        lastPos = 0;
+        
+        for (int i = 0; i < content.length(); i++) {
+            char c = content.charAt(i);
+            if (Character.isWhitespace(c)) {
+            if (i > lastPos) {
+                // Question : i ou i-1 ? vérifier la doc ;)
+                tokens[index++] = content.substring(lastPos, i);
+            }
+            lastPos = i + 1;
+            }
+        }
+        
+        if (lastPos < content.length()) {
+            tokens[index++] = content.substring(lastPos);
+        }
+
+        // --- Extraction des informations ---
+        
+        int pos = 0;
+        String magic = tokens[pos++];
+        
+        // Pourrais être fait au tout début
+        if (!magic.equals("P3")) {
+            throw new IOException("Format PPM non supporté : " + magic);
+        }
+
+        int width = Integer.parseInt(tokens[pos++]);
+        int height = Integer.parseInt(tokens[pos++]);
+        int maxVal = Integer.parseInt(tokens[pos++]); // ex: 255
+
+        Image img = new Image(width, height);
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+            int r = Integer.parseInt(tokens[pos++]);
+            int g = Integer.parseInt(tokens[pos++]);
+            int b = Integer.parseInt(tokens[pos++]);
+            img.setPixel(x, y, r, g, b);
+            }
+        }
+
+        return img;
+    }
+
+
+
+
+
+    static public save_bin (String filename) throws IOException {
+        FileInputStream fs = new FileInputStream(filename);
     }
 }
