4955d3e7d47de6123a2ae40691a88aca808951e7
Xelo574LSvr <leo.sauvaire@iut-rodez.fr>
2025-10-17 09:07:24 +0000

    calcul avec les threads

--- Windows_SAUVAIRE_LEO/Parallelisation/CalculeParallele.java
+++ Windows_SAUVAIRE_LEO/Parallelisation/CalculeParallele.java
@@ -1,5 +1,31 @@
-public class CalculeParallele {
-    Calcule[] threads = new Calcule[DONNEES.length];
+public class CalculParallele {
+    private static final int[] DONNEES = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    private static final int MULTIPLICATEUR = 1000;
 
+    public static void main(String[] args) {
+        System.out.println("=== Calcul Séquentiel ===");
 
+        long debut = System.nanoTime();
+        long sommeTotal = 0;
+
+        Calcule[] threads = new Calcule[DONNEES.length];
+
+        // TODO: Ce code sera remplacé par une version parallèle
+         for (int i = 0; i < DONNEES.length; i++) {
+            threads[i] = new Calcule(DONNEES[i], MULTIPLICATEUR, i);
+            threads[i].start();
+        }
+
+        
+
+        long somme = 0;
+        for (int i = 0; i < DONNEES.length; i++) {
+            threads[i].join();
+            somme += threads[i].getResultat();
+        }
+
+        long duree = (System.nanoTime() - debut) / 1_000_000;
+        System.out.println("Résultat total : " + sommeTotal);
+        System.out.println("Durée : " + duree + " ms");
+    }
 }
