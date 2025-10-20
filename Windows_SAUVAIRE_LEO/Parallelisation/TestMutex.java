2fa62b30aac2a72a2af93d92df01aaad829d91f0
Xelo574LSvr <leo.sauvaire@iut-rodez.fr>
2025-10-20 14:09:56 +0000

    test avec compteur sécurisé

---
+++ Windows_SAUVAIRE_LEO/Parallelisation/TestMutex.java
@@ -0,0 +1,33 @@
+public class TestMutex {
+    private static final int NB_THREADS = 20;
+    private static final int NB_INCREMENT = 100000;
+    private static Thread[] threads = new Thread[NB_THREADS];
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Test du Compteur Securisé");
+
+        long attendu;
+        int obtenu;
+        CompteurSecurise.resetCompteur();
+
+        
+        for (int i = 0; i < NOMBRE_THREADS; i++) {
+            threads[i] = new Thread(new CompteurSecurise.Incrementeur("Thread " + i, NB_INCREMENT));
+            threads[i].start();
+        }
+
+        for (int i = 0; i < NOMBRE_THREADS; i++) {
+            threads[i].join();
+        }
+
+        System.out.println("Compteur : " + CompteurSecurise.getCompteur());
+
+        // Affichage des résultats
+        attendu = (long)NB_THREADS * NB_INCREMENT;
+        obtenu = CompteurSecurise.getCompteur();
+        
+        System.out.println("Résultat attendu : " + attendu);
+        System.out.println("Résultat obtenu  : " + obtenu);
+        System.out.println("Différence       : " + (attendu - obtenu));
+    }
+}
